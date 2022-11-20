99e401c093eb0780996aa1836bb6525b8fb90461
Homero José <96498455+rox651@users.noreply.github.com>
2022-11-20 15:41:19 -0500

    change nav color

--- src/layout/LinkHeader.jsx
+++ src/layout/LinkHeader.jsx
@@ -8,18 +8,22 @@ export const LinkHeader = ({ link }) => {
    const { name, redir } = link;
    const { isDark } = useContext(AppContext);
 
-   const textColor = isDark ? "#fff" : "#000";
-
    return (
       <NavLink
          className={({ isActive }) =>
             isActive
-               ? ` text-[#DF9EF4]  text-center max-w-max relative font-normal  lg:min-h-5 lg:font-semibold lg:px-2 lg:overflow-hidden lg:rounded-xl  lg:border-[1px] lg:border-[#DF9EF4] `
-               : `text-[${textColor}] relative group hover:text-[#DF9EF4] font-normal lg:font-semibold transition-colors lg:min-h-5  lg:overflow-y-clip `
+               ? `text-[#DF9EF4] text-center max-w-max relative font-normal  lg:min-h-5 lg:font-semibold lg:px-2 lg:overflow-hidden lg:rounded-xl  lg:border-[1px] lg:border-[#DF9EF4] `
+               : `${
+                    isDark ? "text-white" : "text-black"
+                 } relative group hover:text-[#DF9EF4] font-normal lg:font-semibold transition-colors lg:min-h-5  lg:overflow-y-clip `
          }
          to={redir}
       >
-         <span className="text-7xl lg:text-base lg:inline-block lg:group-hover:-translate-y-8 transition-transform">
+         <span
+            className={
+               "text-7xl  lg:text-base lg:inline-block lg:group-hover:-translate-y-8 transition-transform"
+            }
+         >
             {name}
          </span>
          <span className="hidden lg:inline-block absolute left-0 translate-y-8 group-hover:translate-y-0 transition-transform">
